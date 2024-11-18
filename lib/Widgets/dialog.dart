e795789dec74f8bc05b2bf320a142662908b6203
Aditya Singh Borana <aditya@ogresto.com>
2024-11-19 00:54:18 +0530

    working app

---
+++ lib/Widgets/dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../helper/functions.dart';
+
+Future<void> dialogBuilder(BuildContext context, String title, String content) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title), // Use the title parameter here
+        content: Container(
+          width: MediaQuery.of(context).size.width -
+              MediaQuery.of(context).size.width / 3,
+          child: Text(content), // Use the content parameter here
+        ),
+        actions: <Widget>[
+          Center(
+            child: InkWell(
+              child: const Text(
+                'Disable',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                route.remove(context);
+              },
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
